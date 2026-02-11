07e22b642c0c34dd9bec484de966b972e90607c7
Ted Martinez <tedmartinez@Teds-MacBook-Pro.local>
2026-02-10 23:02:18 -0700

    add redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://paintinnovators.com/locations/colorado/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
